a856bb127dec34bbd4b8e7f013be5b91c83dda1e
heschwerdt <henry.schwerdtner@campus.tu-berlin.de>
2021-01-31 17:31:32 +0100

    added basic permission UI and a URI for fetching

--- pages/permissions.vue
+++ pages/permissions.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    Hallo Welt
+    <h1 class="title">
+      Permissions
+    </h1>
+    <v-divider />
+    <div class="permission-container">
+      <PConsentList :consents="consents" />
+    </div>
   </div>
 </template>
 <script>
 export default {
+  async asyncData ({ $axios }) {
+    const result = await $axios.$get('/api/permissions')
+    return {
+      consents: result
+    }
+  }
 }
 </script>
+<style scoped>
+.permission-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
