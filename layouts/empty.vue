6226c7559867866a6f4f9aae6b38eff7fb9c3ca6
naseemshawarba <naseem.shawarba@campus.tu-berlin.de>
2022-04-07 20:43:41 +0200

    Fixed NUXTLINK, it can now load the home page

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div {
+    text-align: center;
+    padding: 50px;
+}
+</style>
