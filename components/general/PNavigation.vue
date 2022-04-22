54379fbaa6747230f6ebd8b1ecefe7cb85131c9f
naseemshawarba <naseem.shawarba@campus.tu-berlin.de>
2022-04-22 19:50:19 +0200

    fixed a bug

--- components/general/PNavigation.vue
+++ components/general/PNavigation.vue
@@ -6,8 +6,8 @@
           {{ $t('sidebar-title.navigation') }}
         </v-list-item-title>
         <v-card>
-          <v-list-item-group v-model="group" color="primary">
-            <v-list-item v-for="(link, index) in links" :key="index" :index-v-step="index" :to="localePath(link.to)" exact>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="link in links" :key="$t('links.'+link.label)" :to="localePath(link.to)" exact>
               <v-list-item-title>
                 {{ $t('links.'+link.label) }}
               </v-list-item-title>
@@ -20,21 +20,27 @@
           {{ $t('sidebar-title.update') }}
         </v-list-item-title>
         <v-card>
-          <v-list-item-group v-model="group" color="primary">
-            <v-list-item v-for="(link, index) in linksUpdate" :key="index" :index-v-step="index" :to="localePath(link.to)" exact>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="link in linksUpdate" :key="$t('linksUpdate.'+link.label)" @click="counter = $t('linksUpdate.'+link.label)" :to="localePath('/consent?tab=consent'+'&selectEvent='+$t('linksUpdate.'+link.label))" exact>
               <v-list-item-title>
                 {{ $t('linksUpdate.'+link.label) }}
               </v-list-item-title>
             </v-list-item>
           </v-list-item-group>
-        </v-card>
+       </v-card>
       </v-list>
+      <p>{{ counter }}</p>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      counter: ''
+    }
+  },
   props: {
     links: {
       type: Array,
