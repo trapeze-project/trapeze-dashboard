7a47f2b58b683e54363ee262e4793e543ffbdfb1
naseemshawarba <naseem.shawarba@campus.tu-berlin.de>
2022-04-28 21:01:46 +0200

    added carousel to consent helper

--- pages/helper.vue
+++ pages/helper.vue
@@ -3,32 +3,30 @@
     <h2>
       {{ $t('consent.title') }}
     </h2>
-    <v-card>
-      <v-card-title style="background-color: #F5F5F5">
-        {{ $t('consent.location') }}
-      </v-card-title>
-      <v-divider />
-      <v-card-text>
-        {{ $t('consent.text') }}
-      </v-card-text>
-      <p class="ml-4 font-weight-bold">
-        {{ $t('consent.purpose') }}
-      </p>
-      <v-row>
-        <v-col>
-          <PDataCategoryHelper
-            :categories="categories"
-          />
-        </v-col>
-      </v-row>
-      <div class="text-center">
-        <v-pagination
-          v-model="page"
-          :length="4"
-          circle
-        />
-      </div>
-    </v-card>
+    <v-carousel hide-delimiters light height="400" :show-arrows="false" v-model="page">
+      <v-carousel-item v-for="datatyp in dataTypes" :key="datatyp">
+        <v-card>
+          <v-card-title style="background-color: #F5F5F5">
+            {{ $t('consent.'+datatyp) }}
+          </v-card-title>
+          <v-divider />
+          <v-card-text>
+            {{ $t('consent.text') }}
+          </v-card-text>
+          <p class="ml-4 font-weight-bold">
+            {{ $t('consent.purpose') }}
+          </p>
+          <PDataCategoryHelper :categories="categories" />
+        </v-card>
+      </v-carousel-item>
+    </v-carousel>
+    <div class="text-center">
+      <v-btn @click="page--" class="grey darken-2 custom-transform-class text-none white--text" :disabled="page===0">Back</v-btn>
+      <v-btn @click="page++" class="primary custom-transform-class text-none white--text" :disabled="page===dataTypes.length-1">Next</v-btn>
+    </div>
+    <div class="text-center mb-3">
+      Page {{page+1}}/{{dataTypes.length}}
+    </div>
   </div>
 </template>
 
@@ -38,7 +36,8 @@ export default {
   data () {
     return {
       categoryTitle: 'consent.purpose',
-      page: 1,
+      dataTypes: ['location', 'bank', 'credit', 'fingerprint', 'name'],
+      page: 0,
       categories: ['Advertising', 'Marketing', 'Identity Verification'],
       sensitivity: 30
     }
