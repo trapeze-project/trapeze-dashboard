68efc953e9f812d2e0da638b8e55aeb48c27481b
naseemshawarba <naseem.shawarba@campus.tu-berlin.de>
2022-07-11 13:36:55 +0200

    Changed 'Undo changes' to 'Undo last change'

--- components/consent/PReceiptBtns.vue
+++ components/consent/PReceiptBtns.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="float-right">
     <v-btn class="white--text" color="red" @click="undoChanges">
-      {{ $t("btn.labels.undo-changes") }}
+      {{ $t("btn.labels.undo-last-change") }}
     </v-btn>
     <!-- <v-btn class="mx-3">{{$t("btn.labels.see-changes")}}</v-btn> -->
     <v-btn class="black--text" color="primary" @click="submitChanges">
