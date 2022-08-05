65148f61003c1c81581dbc1f08641aaf337c5521
naseemshawarba <naseem.shawarba@tu-berlin.de>
2022-08-05 12:09:31 +0200

    fixed styling issues

--- components/shared/PAlertLeaveDialog.vue
+++ components/shared/PAlertLeaveDialog.vue
@@ -1,35 +1,34 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title class="text-h5">
-          {{ this.$t("PAlertLeaveDialog.dialog-title") }}
-        </v-card-title>
-        <v-card-text v-html="this.alertBody" />
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="green darken-1"
-            text
-            @click="()=>{leaveAnyWay = false; dialog=false; }"
-          >
-            {{ this.$t("btn.labels.cancel") }}
-          </v-btn>
-          <v-btn
-            color="green darken-1"
-            text
-            @click="()=>{leaveAnyWay = true; dialog=false; }"
-          >
-            {{ this.$t("btn.labels.leave-anyway") }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="350"
+  >
+    <v-card class="alertCard">
+      <v-card-title class="text-h5 " style="word-break:break-word">
+        <p class="">{{ this.$t("PAlertLeaveDialog.dialog-title") }}</p>
+      </v-card-title>
+      <v-card-text v-html="this.alertBody" />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="green darken-1"
+          text
+          @click="()=>{leaveAnyWay = false; dialog=false; }"
+        >
+          {{ this.$t("btn.labels.cancel") }}
+        </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="()=>{leaveAnyWay = true; dialog=false; }"
+        >
+          {{ this.$t("btn.labels.leave-anyway") }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
 </template>
 
 <script>
@@ -50,3 +49,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+</style>
