e4d13327728aee6cb3d97b644b0c0f5455d50f96
Philip Raschke <philip.raschke@tu-berlin.de>
2022-08-16 13:26:50 +0200

    task done: Tooltip UI implementieren, beispielhaft f?r "Consent" und "Purpose", sollen ?bersetzbar sein [see https://vuetifyjs.com/en/components/tooltips/ ]

--- components/layout/PNavigation.vue
+++ components/layout/PNavigation.vue
@@ -7,21 +7,23 @@
             <v-subheader>
               {{ $t("sidebar-title.navigation") }}
             </v-subheader>
-            <div
-              v-for="link in links"
-              :key="link.label"
-            >
-              <v-list-group
-                v-if="link.subLinks"
-                color="lightgrey"
-              >
+            <div v-for="link in links" :key="link.label">
+
+              <v-list-group v-if="link.subLinks" color="lightgrey">
+
                 <template v-slot:activator>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      {{ $t(link.label) }}
-                    </v-list-item-title>
-                  </v-list-item-content>
+                  <v-tooltip right>
+                    <template v-slot:activator="{ on }">
+                      <v-list-item-content v-on="on">
+                        <v-list-item-title>
+                          {{ $t(link.label) }}
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                    <span> {{ $t(link.label) }} </span>
+                  </v-tooltip>
                 </template>
+
                 <v-list-item
                   v-for="sub in link.subLinks"
                   :key="sub.label"
@@ -29,20 +31,35 @@
                   :to="localePath(localePath(sub.to))"
                   exact
                 >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      {{ $t(sub.label) }}
-                    </v-list-item-title>
-                  </v-list-item-content>
+
+                  <v-tooltip right>
+                    <template v-slot:activator="{ on }">
+                      <v-list-item-content v-on="on">
+                        <v-list-item-title>
+                          {{ $t(sub.label) }}
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                    <span> {{ $t(sub.label) }} </span>
+                  </v-tooltip>                  
+
                 </v-list-item>
+
               </v-list-group>
+
               <v-list-item v-else :to="localePath(link.to)" exact>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ $t(link.label) }}
-                  </v-list-item-title>
-                </v-list-item-content>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-list-item-content v-on="on">
+                      <v-list-item-title>
+                        {{ $t(link.label) }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <span> {{ $t(link.label) }} </span>
+                </v-tooltip>
               </v-list-item>
+
             </div>
           </v-list>
         </v-card>
@@ -57,32 +74,34 @@ export default {
     group: {
       type: null,
       required: false,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
       links: [
-        { to: '/', label: 'nav.labels.home' },
+        { to: "/", label: "nav.labels.home" },
         {
-          to: '/consent?tab=consent',
-          label: 'nav.labels.consent-menu',
+          to: "/consent?tab=consent",
+          label: "nav.labels.consent-menu",
           subLinks: [
-            { to: '/consent?tab=data', label: 'nav.labels.data' },
-            { to: '/consent?tab=purpose', label: 'nav.labels.purposes' },
-            { to: '/consent?tab=consent', label: 'nav.labels.consent' },
-          ]
+            { to: "/consent?tab=data", label: "nav.labels.data" },
+            { to: "/consent?tab=purpose", label: "nav.labels.purposes" },
+            { to: "/consent?tab=consent", label: "nav.labels.consent" },
+          ],
         },
-        { to: '/help', label: 'nav.labels.help' },
-        { to: '/faq', label: 'nav.labels.faq' }
-      ]
-    }
-  }
-}
+        { to: "/help", label: "nav.labels.help" },
+        { to: "/faq", label: "nav.labels.faq" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-.v-list-group .v-list-group__header .v-list-item__icon.v-list-group__header__append-icon {
+.v-list-group
+  .v-list-group__header
+  .v-list-item__icon.v-list-group__header__append-icon {
   min-width: inherit;
   justify-content: flex-start;
 }
