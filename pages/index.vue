b876cf46ddcfa1d905eddccf50ede04e2de63de4
Philip Raschke <philip.raschke@tu-berlin.de>
2023-01-13 14:47:48 +0100

    cleaned index page; needs to adapt new mock up workflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,10 @@
 <template>
   <div>
-    <v-btn @click="loadOrgDashboard('CloverIT')">
-      CloverIT
-    </v-btn>
-    <v-btn @click="loadOrgDashboard('AlexIT')">
-      AlexIT
-    </v-btn>
   </div>
 </template>
 
 <script>
-/*
-import AlexITController from '~/static/data/AlexITController.json'
-import CloverITController from '~/static/data/CloverITController.json'
-
 export default {
   layout: 'default',
-  methods:{
-    loadOrgDashboard(organizationName){
-      if(organizationName === 'CloverIT'){
-        this.$GlobalVariables.dataController = CloverITController
-        this.$router.push({ path:this.localePath(`/${organizationName}`) } )
-      }else if(organizationName === 'AlexIT'){
-        this.$GlobalVariables.dataController = AlexITController
-        this.$router.push({ path:this.localePath(`/${organizationName}`)})
-      }
-
-    }
-  },
 }
-*/
 </script>
