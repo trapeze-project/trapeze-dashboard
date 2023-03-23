fc41c5670700d961929c3aa130be4570680a0984
Philip Raschke <philip.raschke@tu-berlin.de>
2023-03-23 11:03:46 +0100

    improved landing page

--- pages/index.vue
+++ pages/index.vue
@@ -20,97 +20,51 @@
 
         <v-row class="mt-5">
 
-          <v-col 
-            cols="10"
+          <v-col
+            v-for="card, index in cards"
+            :key="index"
+            cols="12"
             md="4"
-            offset="1"
-            offset-md="0"
-            class="pa-8" 
+            class="pa-4"
             align="left"
           >
             <v-card rounded="xl" elevation="3" class="d-flex flex-column fill-height">
               <v-card-title>
-                <v-icon color="black" size="48" class="mr-3">mdi-account-circle-outline</v-icon>
-                {{ $t("landing.login.heading") }}
+                <!-- <v-icon color="black" size="48" class="mr-3">mdi-account-circle-outline</v-icon> -->
+                {{ $t(card.heading) }}
+                <v-img :src="card.img" class="mt-5 mx-auto" max-width="85%" />
               </v-card-title>
-
-              <v-card-text>
-                {{ $t("landing.login.content") }}
-              </v-card-text>
-
+              
               <v-spacer />
 
+              <v-card-text>
+                {{ $t(card.content) }}
+              </v-card-text>
+
               <v-card-actions class="d-flex justify-center pb-3">
-                <v-btn color="primary" class="rounded-pill px-5 black--text" disabled>
-                  {{ $t("landing.login.btn") }}
+                
+                <v-btn
+                  v-if="card.to"
+                  color="primary"
+                  class="rounded-pill px-5 black--text"
+                  :to="card.to"
+                >
+                  {{ $t(card.btn) }}
                 </v-btn>
+
+                <v-btn
+                  v-else 
+                  color="primary"
+                  class="rounded-pill px-5 black--text"
+                  disabled
+                >
+                  {{ $t(card.btn) }}
+                </v-btn>
+
               </v-card-actions>
             </v-card>
           </v-col>
 
-          <v-col 
-            cols="10"
-            md="4"
-            offset="1"
-            offset-md="0"
-            class="pa-2"
-            align="left"
-          >
-            <v-card
-              rounded="xl"
-              elevation="5"
-              class="d-flex flex-column fill-height"
-            >
-              <v-card-title>
-                <v-icon color="black" size="48" class="mr-3">mdi-domain</v-icon>
-                {{ $t("landing.choose-controller.heading") }}
-              </v-card-title>
-
-              <v-card-text>
-                {{ $t("landing.choose-controller.content") }}
-              </v-card-text>
-
-              <v-spacer />
-
-              <v-card-actions class="d-flex justify-center pb-3">
-                <v-btn 
-                  color="primary"
-                  class="rounded-pill px-5 black--text" 
-                  :to="`/${$i18n.locale}/controller-selection`"
-                >
-                  {{ $t("landing.choose-controller.btn") }}
-                </v-btn>
-              </v-card-actions>              
-            </v-card>
-          </v-col>
-
-          <v-col 
-            cols="10"
-            md="4"
-            offset="1"
-            offset-md="0"
-            class="pa-8"
-            align="left"
-          >
-            <v-card rounded="xl" elevation="3" class="d-flex flex-column fill-height">
-              <v-card-title>
-                <v-icon color="black" size="48" class="mr-3">mdi-file-sign</v-icon>
-                {{ $t("landing.consent.heading") }}
-              </v-card-title>
-
-              <v-card-text>
-                {{ $t("landing.consent.content") }}
-              </v-card-text>
-
-              <v-spacer />
-
-              <v-card-actions class="d-flex justify-center pb-3">
-                <v-btn color="primary" class="rounded-pill px-5 black--text" disabled>
-                  {{ $t("landing.consent.btn") }}
-                </v-btn>
-              </v-card-actions>              
-            </v-card>
-          </v-col>
         </v-row>        
 
       </v-col>
@@ -126,7 +80,7 @@
         class="mt-3 mb-5"
       >
         <v-img src="/img/eu-logo.png" width="48" class="mb-5" />
-        The TRAPEZE project receives funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 883464. Copyright © 2023
+        <small>The TRAPEZE project receives funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 883464. Copyright © 2023</small>
       </v-col>
     </v-row>
   </v-container>
@@ -135,10 +89,31 @@
 <script>
 export default {
   layout: "appBarOnly",
-  methods: {
-    redirect() {
-
+  data() {
+    return {
+      cards: []
     }
+  },
+  mounted() {
+    this.cards = [{
+        heading: "landing.login.heading",
+        content: "landing.login.content",
+        btn: "landing.login.btn",
+        to: null,
+        img: "/img/landing-login-icon.svg"
+      }, {
+        heading: "landing.choose-controller.heading",
+        content: "landing.choose-controller.content",
+        btn: "landing.choose-controller.btn",
+        to: `/${this.$i18n.locale}/controller-selection`,
+        img: "/img/landing-select-icon.svg"
+      }, {
+        heading: "landing.consent.heading",
+        content: "landing.consent.content",
+        btn: "landing.consent.btn",
+        to: null,
+        img: "/img/landing-manage-icon.svg"
+      }]
   }
 }
 </script>
