040ad1e5c14401c5f3bf79f41c37a18e8001adb6
naseemshawarba <naseem.shawarba@tu-berlin.de>
2023-03-31 12:22:32 +0200

    improved the visuals of the 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,85 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink :to="localePath('/')">
-      Home page
-    </NuxtLink>
+    <PNavBar />
+    <v-main>
+      <div class="bg"></div>
+      <div class="bg-text">
+        <p class='error-code'>{{this.error.statusCode}}</p>
+        <p class="error-text">{{this.title}}</p>    
+        <NuxtLink :to="localePath('/')">
+          <p class="action">{{$t('error.go-to-home')}}</p>
+        </NuxtLink>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: "empty",
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      title:""
+    };
   },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+  head() {
     return {
-      title
-    }
+      title: this.title
+    };
+  },
+  created(){
+    const pageNotFound = this.$t('error.page-not-found')
+    const otherError = this.$t('an-error-occurred')
+    this.title =
+      this.error.statusCode === 404 ? pageNotFound : otherError;
   }
-}
+};
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+
+
+
+.bg {
+  background-image: url("/img/bg.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  filter: blur(6px);
+  -webkit-filter: blur(6px);
+  /* Center and scale the image nicely */
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
+.bg-text {
+  color: black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.error-code {
+  font-size: 13em;
+  font-weight: bold;
+  margin-bottom: 0px;
+}
+.error-text {
+  font-size: 2em;
+  font-weight: bold;
+}
+.action {
+  font-size: 2em;
+}
+
 </style>
