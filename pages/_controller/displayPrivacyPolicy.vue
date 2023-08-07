18a1f6a285598fd1a8477d2a793489ffa15cc8cf
naseemshawarba <naseem.shawarba@tu-berlin.de>
2023-08-07 21:32:19 +0200

    added download button to displayPP page; fixed bug(controllers images missing)

--- pages/_controller/displayPrivacyPolicy.vue
+++ pages/_controller/displayPrivacyPolicy.vue
@@ -5,6 +5,15 @@
     </div>
     <p v-else-if="$fetchState.error">An error occurred :(</p>
     <div v-else>
+      <v-container fluid >
+        <v-row justify="end" class="ma-1">
+          <v-btn color="green" @click="download">
+            Download
+            <v-icon dark> mdi-file-download </v-icon>
+          </v-btn>
+        </v-row>
+      </v-container>
+
       <pre class="ma-2" v-text="formatedPrivacyPolicy"></pre>
     </div>
   </div>
@@ -28,15 +37,26 @@ export default {
   async fetch() {
     let controllerPolicyRequestBody = this.getControllerPolicyRequestBody();
 
-    let policy = await PolicyService.default.get(
-      controllerPolicyRequestBody
+    let policy = await PolicyService.default.get(controllerPolicyRequestBody);
+    this.formatedPrivacyPolicy = JSON.stringify(
+      policy.getControllerPolicy(),
+      undefined,
+      6
     );
-    this.formatedPrivacyPolicy = JSON.stringify(policy.getControllerPolicy(), undefined,6);
-
   },
   methods: {
     getControllerPolicyRequestBody() {
       return this.controller.controllerPolicyRequestBody;
+    },
+    download(){
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(new Blob([this.formatedPrivacyPolicy], {
+        type: "application/ld+json"
+      }));
+      a.setAttribute("download", "privacyPolicy.jsonld");
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
     },
   },
 };
